4138979e429ef3636a5d6869558abb427afcaef7
duston <>
2024-05-08 01:02:45 -0500

    open a file once in visual studio and break tabs

--- src/Camera.cs
+++ src/Camera.cs
@@ -2,33 +2,33 @@
 
 public class Camera : Component
 {
-    public static Camera Main;
-    public float FieldOfView = 90;
-    public Matrix4 ViewMatrix;
-    public Matrix4 ProjectionMatrix;
+	public static Camera Main;
+	public float FieldOfView = 90;
+	public Matrix4 ViewMatrix;
+	public Matrix4 ProjectionMatrix;
 
-    protected override void OnAttached()
-    {
-        Main = this;
-        ViewMatrix = Matrix4.LookAt( Transform.Position, Vector3.Zero, Axis.Up );
-        ProjectionMatrix = Matrix4.CreatePerspectiveFieldOfView(
-            MathHelper.DegreesToRadians( FieldOfView ),
-            Engine.ScreenSize.X / (float)Engine.ScreenSize.Y,
-            0.1f,
-            300.0f
-        );
-    }
+	protected override void OnAttached()
+	{
+		Main = this;
+		ViewMatrix = Matrix4.LookAt( Transform.Position, Vector3.Zero, Axis.Up );
+		ProjectionMatrix = Matrix4.CreatePerspectiveFieldOfView(
+			MathHelper.DegreesToRadians( FieldOfView ),
+			Engine.ScreenSize.X / (float)Engine.ScreenSize.Y,
+			0.1f,
+			300.0f
+		);
+	}
 
-    public override void Render()
-    {
-        base.Render();
-        ViewMatrix = Matrix4.LookAt( Transform.Position, Transform.Position + Transform.Forward, Axis.Up );
+	public override void Render()
+	{
+		base.Render();
+		ViewMatrix = Matrix4.LookAt( Transform.Position, Transform.Position + Transform.Forward, Axis.Up );
 
-        ProjectionMatrix = Matrix4.CreatePerspectiveFieldOfView(
-            MathHelper.DegreesToRadians( FieldOfView ),
-            Engine.ScreenSize.X / (float)Engine.ScreenSize.Y,
-            0.1f,
-            300.0f
-        );
-    }
+		ProjectionMatrix = Matrix4.CreatePerspectiveFieldOfView(
+			MathHelper.DegreesToRadians( FieldOfView ),
+			Engine.ScreenSize.X / (float)Engine.ScreenSize.Y,
+			0.1f,
+			300.0f
+		);
+	}
 }
